f3a21de113e073f60403cb6a971967fc132daf7c
Bhuvan Thirwani <43582578+bhuvanthirwani@users.noreply.github.com>
2024-10-22 14:49:48 -0400

    Time: 241 ms (96.59%) | Memory: 63.3 MB (5.05%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        directed_graph = {}
+        undirected_graph = {}
+        for connection in connections:
+            if connection[0] not in directed_graph:
+                directed_graph[connection[0]] = {}
+            if connection[1] not in directed_graph:
+                directed_graph[connection[1]] = {}
+            directed_graph[connection[0]][connection[1]] = 1
+            if connection[0] not in undirected_graph:
+                undirected_graph[connection[0]] = {}
+            if connection[1] not in undirected_graph:
+                undirected_graph[connection[1]] = {} 
+            undirected_graph[connection[0]][connection[1]] = 1
+            undirected_graph[connection[1]][connection[0]] = 1
+        
+        visited = set()
+        global count
+        count = 0
+        def dfs(s):
+            global count
+            visited.add(s)
+            for u in undirected_graph[s]:
+                if u not in visited:
+                    if s not in directed_graph[u]:
+                        count += 1
+                    dfs(u)
+            return
+        dfs(0)
+        return count
