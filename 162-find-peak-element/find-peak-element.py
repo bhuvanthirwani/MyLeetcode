34bcdc5cbeb2d75176a77ab592b3112c4b5d6ab9
Bhuvan Thirwani <43582578+bhuvanthirwani@users.noreply.github.com>
2024-10-23 17:20:09 -0400

    Time: 0 ms (100.00%) | Memory: 16.8 MB (33.50%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        def binary_search(l, r, n):
+            nonlocal nums
+            while(l<=r):
+                mid = (l+r)//2
+                print("mid: ", mid)
+                if mid==n-1:
+                    if nums[mid] < nums[mid-1]:
+                        return mid-1
+                    else:
+                        return mid
+                if mid==0:
+                    if nums[mid] < nums[mid+1]:
+                        return mid+1
+                    else:
+                        return mid
+                if (nums[mid] > nums[mid-1]) and (nums[mid] > nums[mid+1]):
+                    return mid
+                
+                if nums[mid] <= nums[mid+1]:
+                    l=mid+1
+                else:
+                    r=mid-1
+        return binary_search(0, n-1, n)
