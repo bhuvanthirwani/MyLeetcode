cf5f6a8fe2a6d662f4a64af0214fe0b8a67d0024
Bhuvan Thirwani <bhuvanthirwani@gmail.com>
2025-02-16 22:57:30 -0500

    Time: 15 ms (82.53%) | Memory: 17.8 MB (82.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,7 +4,6 @@ class Solution:
         D=dict()
         i,j=0,0
         mv=0
-        flag=0
         while(j<l):
             if(s[j] not in D):
                 D[s[j]]=j
@@ -15,9 +14,5 @@ class Solution:
                     D.pop(s[i])
                     i+=1
                 D[s[j]]=j
-            #print(D)
-
             j+=1
-            if flag == 1:
-                return 0
         return mv
